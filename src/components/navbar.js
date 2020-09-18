e7dbfd1167ac6afe3d4c25b7db80ab6dc72f7fd5
tiagoMurer <tiagostaurenghi@hotmail.com>
2020-09-18 15:46:14 -0300

    novo menu mobile

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -23,60 +23,88 @@ class Navbar extends Component  {
     render() {
 
         return(
-            <nav className="navbar">
-                <div className="navbar__logo">
-                    <h2>Tiago Murer</h2>
-                </div>
-
-                <div className="navbar__info desktop">
-                    <Link to="/"><span>Quem sou eu</span> </Link>
-                    <Link to="/"><span>Projetos</span> </Link>
-                    <Link to="/"> <span>Contato</span> </Link>
-                </div>
-
-                <div className="navbar__links desktop">
-                    <a href="https://github.com/tsmurer" target="_blank" rel="noreferrer">
-                    <svg 
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='45'
-                        height='45'
-                        viewBox='0 0 512 512'
-                        className="navbar__links--github"
-                    >
-                        <title>ionicons-v5_logos</title>
-                        <path d='M256,32C132.3,32,32,134.9,32,261.7c0,101.5,64.2,187.5,153.2,217.9a17.56,17.56,0,0,0,3.8.4c8.3,0,11.5-6.1,11.5-11.4,0-5.5-.2-19.9-.3-39.1a102.4,102.4,0,0,1-22.6,2.7c-43.1,0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1,1.4-14.1h.1c22.5,2,34.3,23.8,34.3,23.8,11.2,19.6,26.2,25.1,39.6,25.1a63,63,0,0,0,25.6-6c2-14.8,7.8-24.9,14.2-30.7-49.7-5.8-102-25.5-102-113.5,0-25.1,8.7-45.6,23-61.6-2.3-5.8-10-29.2,2.2-60.8a18.64,18.64,0,0,1,5-.5c8.1,0,26.4,3.1,56.6,24.1a208.21,208.21,0,0,1,112.2,0c30.2-21,48.5-24.1,56.6-24.1a18.64,18.64,0,0,1,5,.5c12.2,31.6,4.5,55,2.2,60.8,14.3,16.1,23,36.6,23,61.6,0,88.2-52.4,107.6-102.3,113.3,8,7.1,15.2,21.1,15.2,42.5,0,30.7-.3,55.5-.3,63,0,5.4,3.1,11.5,11.4,11.5a19.35,19.35,0,0,0,4-.4C415.9,449.2,480,363.1,480,261.7,480,134.9,379.7,32,256,32Z'/>
-                    </svg> {/* Baixado em https://ionicons.com/ */} 
-                    </a>
-
-                    <a href="https://www.linkedin.com/in/tiagomurer/" target="_blank" rel="noreferrer">
+            <div className="componentContainer">
+                <nav className="mobileMenu">
                     <svg
                         xmlns='http://www.w3.org/2000/svg'
                         width='45'
                         height='45'
                         viewBox='0 0 512 512'
-                        className="navbar__links--linkedin"
+                        className="mobileMenu__closeMobile"
+                        // onClick={ }
                     >
-                            <title>ionicons-v5_logos</title>
-                            <path d='M444.17,32H70.28C49.85,32,32,46.7,32,66.89V441.61C32,461.91,49.85,480,70.28,480H444.06C464.6,480,480,461.79,480,441.61V66.89C480.12,46.7,464.6,32,444.17,32ZM170.87,405.43H106.69V205.88h64.18ZM141,175.54h-.46c-20.54,0-33.84-15.29-33.84-34.43,0-19.49,13.65-34.42,34.65-34.42s33.85,14.82,34.31,34.42C175.65,160.25,162.35,175.54,141,175.54ZM405.43,405.43H341.25V296.32c0-26.14-9.34-44-32.56-44-17.74,0-28.24,12-32.91,23.69-1.75,4.2-2.22,9.92-2.22,15.76V405.43H209.38V205.88h64.18v27.77c9.34-13.3,23.93-32.44,57.88-32.44,42.13,0,74,27.77,74,87.64Z'/>
+                    <title>ionicons-v5-l</title>
+                    <line 
+                        x1='368'
+                        y1='368'
+                        x2='144'
+                        y2='144'
+                        style={{fill:'none', stroke: '#fff', strokeLinecap:'round', strokeLinejoin:'round', strokeWidth:'32px'}}
+                    />
+                    <line
+                        x1='368'
+                        y1='144'
+                        x2='144'
+                        y2='368'
+                        style={{fill:'none', stroke: '#fff', strokeLinecap:'round', strokeLinejoin:'round', strokeWidth:'32px'}}
+                    />
                     </svg>
-                    </a>
+                </nav>
+                <nav className="navbar">
+                    <div className="navbar__logo">
+                        <h2>Tiago Murer</h2>
+                    </div>
 
-                </div>
+                    <div className="navbar__info desktop">
+                        <Link to="/"><span>Quem sou eu</span> </Link>
+                        <Link to="/"><span>Projetos</span> </Link>
+                        <Link to="/"> <span>Contato</span> </Link>
+                    </div>
 
-                <svg 
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='45'
-                    height='45'
-                    viewBox='0 0 512 512'
-                    className="mobile"
-                    onClick={ () => {this.openMenu()}}
-                >
-                    <title>ionicons-v5-j</title>
-                    <line x1='88' y1='152' x2='424' y2='152' style= {{fill:'none', stroke: '#000', strokeLinecap: 'round', strokeMiterlimit: '10', strokeWidth: '48px'}} />
-                    <line x1='88' y1='256' x2='424' y2='256' style= {{fill:'none', stroke:'#000', strokeLinecap:'round', strokeMiterlimit:'10', strokeWidth:'48px'}} />
-                    <line x1='88' y1='360' x2='424' y2='360' style= {{fill:'none', stroke:'#000', strokeLinecap:'round', strokeMiterlimit:'10', strokeWidth:'48px'}} />
-                </svg>
-            </nav>
+                    <div className="navbar__links desktop">
+                        <a href="https://github.com/tsmurer" target="_blank" rel="noreferrer">
+                        <svg 
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='45'
+                            height='45'
+                            viewBox='0 0 512 512'
+                            className="navbar__links--github"
+                        >
+                            <title>ionicons-v5_logos</title>
+                            <path d='M256,32C132.3,32,32,134.9,32,261.7c0,101.5,64.2,187.5,153.2,217.9a17.56,17.56,0,0,0,3.8.4c8.3,0,11.5-6.1,11.5-11.4,0-5.5-.2-19.9-.3-39.1a102.4,102.4,0,0,1-22.6,2.7c-43.1,0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1,1.4-14.1h.1c22.5,2,34.3,23.8,34.3,23.8,11.2,19.6,26.2,25.1,39.6,25.1a63,63,0,0,0,25.6-6c2-14.8,7.8-24.9,14.2-30.7-49.7-5.8-102-25.5-102-113.5,0-25.1,8.7-45.6,23-61.6-2.3-5.8-10-29.2,2.2-60.8a18.64,18.64,0,0,1,5-.5c8.1,0,26.4,3.1,56.6,24.1a208.21,208.21,0,0,1,112.2,0c30.2-21,48.5-24.1,56.6-24.1a18.64,18.64,0,0,1,5,.5c12.2,31.6,4.5,55,2.2,60.8,14.3,16.1,23,36.6,23,61.6,0,88.2-52.4,107.6-102.3,113.3,8,7.1,15.2,21.1,15.2,42.5,0,30.7-.3,55.5-.3,63,0,5.4,3.1,11.5,11.4,11.5a19.35,19.35,0,0,0,4-.4C415.9,449.2,480,363.1,480,261.7,480,134.9,379.7,32,256,32Z'/>
+                        </svg> {/* Baixado em https://ionicons.com/ */} 
+                        </a>
+
+                        <a href="https://www.linkedin.com/in/tiagomurer/" target="_blank" rel="noreferrer">
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='45'
+                            height='45'
+                            viewBox='0 0 512 512'
+                            className="navbar__links--linkedin"
+                        >
+                                <title>ionicons-v5_logos</title>
+                                <path d='M444.17,32H70.28C49.85,32,32,46.7,32,66.89V441.61C32,461.91,49.85,480,70.28,480H444.06C464.6,480,480,461.79,480,441.61V66.89C480.12,46.7,464.6,32,444.17,32ZM170.87,405.43H106.69V205.88h64.18ZM141,175.54h-.46c-20.54,0-33.84-15.29-33.84-34.43,0-19.49,13.65-34.42,34.65-34.42s33.85,14.82,34.31,34.42C175.65,160.25,162.35,175.54,141,175.54ZM405.43,405.43H341.25V296.32c0-26.14-9.34-44-32.56-44-17.74,0-28.24,12-32.91,23.69-1.75,4.2-2.22,9.92-2.22,15.76V405.43H209.38V205.88h64.18v27.77c9.34-13.3,23.93-32.44,57.88-32.44,42.13,0,74,27.77,74,87.64Z'/>
+                        </svg>
+                        </a>
+
+                    </div>
+
+                    <svg 
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='45'
+                        height='45'
+                        viewBox='0 0 512 512'
+                        className="mobile"
+                        onClick={ () => {this.openMenu()}}
+                    >
+                        <title>ionicons-v5-j</title>
+                        <line x1='88' y1='152' x2='424' y2='152' style= {{fill:'none', stroke: '#000', strokeLinecap: 'round', strokeMiterlimit: '10', strokeWidth: '48px'}} />
+                        <line x1='88' y1='256' x2='424' y2='256' style= {{fill:'none', stroke:'#000', strokeLinecap:'round', strokeMiterlimit:'10', strokeWidth:'48px'}} />
+                        <line x1='88' y1='360' x2='424' y2='360' style= {{fill:'none', stroke:'#000', strokeLinecap:'round', strokeMiterlimit:'10', strokeWidth:'48px'}} />
+                    </svg>
+                </nav>
+            </div>
         )
     }
 }
