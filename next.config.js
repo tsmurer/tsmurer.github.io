939f805e7461bb300a155337f0db5c860ab840c8
tsmurer <tsmurer@Ubuntu-22.04.2.myguest.virtualbox.org>
2023-10-16 19:04:35 -0300

    dist folder in gitignore

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    trailingSlash: true,
+    output: 'export',
+    distDir: 'dist',
+}
 
 module.exports = nextConfig
