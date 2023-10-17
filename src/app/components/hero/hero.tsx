743f4a1e81f8aa247b6adc1ebc3b81134e7fb369
tsmurer <tsmurer@Ubuntu-22.04.2.myguest.virtualbox.org>
2023-10-16 23:20:33 -0300

    hero card

--- src/app/components/hero/hero.tsx
+++ src/app/components/hero/hero.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './hero.scss'
+import React from 'react';
+import './hero.scss';
+import Button from '@mui/material/Button';
+import { Download, GitHub, LinkedIn } from '@mui/icons-material';
 
 export default function Hero() {
     return <div className="hero">
-        <div className="blur blur--green"></div>
-        <div className="blur blur--blue"></div>
+        <div className="hero-bg"></div>
+        <div className="container">
+            <div className="details1">
+                <h3 className="details1-header1">Hello! I'm a </h3>
+                <h3 className="details1-header2">Software Developer</h3>
+            </div>
+            <div className="details2">
+                <div className="social-media">
+                    <LinkedIn className="icon"/> <GitHub className="icon"/>
+                </div>
+                <Button variant="contained" endIcon={<Download/>}>My Resume</Button>
+            </div>
+            <div className="profpic">
+                <img src="/profPic.jpeg" className="profpic grid-2-col-2-row"/>
+            </div>
+        
+        </div>
     </div>
 }
 
