fc3bad643654140bcdc5aa401d4a5710a47f40dc
tsmurer <tsmurer@Ubuntu-22.04.2.myguest.virtualbox.org>
2023-10-17 09:18:32 -0300

    fix image

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
+    images: { unoptimized: true },
 }
 
 module.exports = nextConfig
--- src/app/components/hero/hero.tsx
+++ src/app/components/hero/hero.tsx
@@ -9,17 +9,15 @@ export default function Hero() {
         <div className="hero-bg"></div>
         <div className="container">
             <div className="details1">
-                <h3 className="details1-header1">Hello! I&apos;m a </h3>
+                <h3 className="details1-header1">Hello, I&apos;m a </h3>
                 <h3 className="details1-header2">Software Developer</h3>
             </div>
             <div className="details2">
-                <div className="social-media">
-                    <LinkedIn className="icon"/> <GitHub className="icon"/>
-                </div>
+                <p>My name is Tiago and I'm a passionate software developer working from Sao Paulo, Brazil.</p>
                 <Button variant="contained" endIcon={<Download/>}>My Resume</Button>
             </div>
             <div className="profpic">
-                <Image src="/profPic.jpeg" alt="profile picture" className="profpic grid-2-col-2-row"/>
+                <Image fill={true} style={{objectFit: "cover"}} src="/profPic.jpeg" alt="profile picture" className="profpic grid-2-col-2-row"/>
             </div>
         
         </div>
