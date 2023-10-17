5f43f4c5c4fded756cafcfe7ececbb1880fcbf0b
tsmurer <tsmurer@Ubuntu-22.04.2.myguest.virtualbox.org>
2023-10-17 15:56:52 -0300

    removal of faulty char

--- src/app/components/about/about.tsx
+++ src/app/components/about/about.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import './about.scss'
 
 export default function about() {
@@ -12,7 +11,7 @@ export default function about() {
         <div className="about__container__text">
             <h2 className="section-title"> About Me</h2>
             <hr></hr>
-            <p>Hello, I&apos;m Tiago, a dedicated programmer with three years of experience in the tech industry. I'm passionate about coding and creating solutions that make a difference.</p>
+            <p>Hello, I&apos;m Tiago, a dedicated programmer with three years of experience in the tech industry. I&apos;m passionate about coding and creating solutions that make a difference.</p>
             <p>My journey in programming in 2018 when I first started to write JavaScript and Python scrips. I had just finished law school when got really curious about programming.</p>
         </div>
     </div>
