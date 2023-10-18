959dea63a0dab3ea9b98d6c0861a4cb962f9278e
tsmurer <tsmurer@Ubuntu-22.04.2.myguest.virtualbox.org>
2023-10-17 21:41:51 -0300

    removing faulty character

--- src/app/components/about/about.tsx
+++ src/app/components/about/about.tsx
@@ -11,7 +11,7 @@ export default function about() {
         <div className="about__container__text">
             <p>Hello, I&apos;m Tiago, a dedicated programmer with three years of experience in the tech industry. I&apos;m passionate about coding and creating solutions that make a difference.</p>
             <p>My journey in programming in 2018 when I first started to write JavaScript and Python scripts. I had just finished law school when got really curious about programming after bumping into Khan Academy&apos;s content on programming.</p>
-            <p>Since then, I finished a new graduation at the Sao Paulo&apos;s Federal Institute of Education, Science and Technology, and I'm on my third job as a software developer, at Tata Consultancy Services.</p>
+            <p>Since then, I finished a new graduation at the Sao Paulo&apos;s Federal Institute of Education, Science and Technology, and I am on my third job as a software developer, at Tata Consultancy Services.</p>
 
         </div>
     </div>
