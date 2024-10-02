8a8403bc12d5348a66da47a96ab99ae0b0c6d17c
Tiago S Murer <tiagostaurenghi@gmail.com>
2024-10-02 14:15:31 -0300

    adding a layout for home and some content

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,37 @@
 const eleventySass = require("eleventy-sass");
+const Image = require("@11ty/eleventy-img");
 
 module.exports = function(eleventyConfig) {
-
-  eleventyConfig.addPlugin(eleventySass);
   
-    return {
-      dir: {
-        input: "src",
-        output: "dist",
-        includes: "_includes"
-      }
+  // Add Sass plugin
+  eleventyConfig.addPlugin(eleventySass);
+
+  // Define a shortcode for images
+  eleventyConfig.addNunjucksAsyncShortcode("image", async (src, alt, widths, formats, className) => {
+    let metadata = await Image(src, {
+      widths: widths || [300, 600],
+      formats: formats || ["jpeg", "webp"],
+      urlPath: "/images/",
+      outputDir: "./dist/images/",
+    });
+
+    let imageAttributes = {
+      alt,
+      sizes: "(min-width: 30em) 50vw, 100vw",
+      loading: "lazy",
+      decoding: "async",
+      class: className // Allow passing class name here
     };
+
+    // Return image HTML
+    return Image.generateHTML(metadata, imageAttributes);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      includes: "_includes"
+    }
   };
+};
