3292e080ae796a1b71a3a0622b08ba81b2dfd597
Tiago S Murer <tiagostaurenghi@gmail.com>
2024-10-02 20:18:56 -0300

    fixed margin

--- src/_data/jobs.js
+++ src/_data/jobs.js
@@ -6,14 +6,14 @@ module.exports = [
         date: "Mar 2019 - Dec 2019",
     },
     {
-      jobTitle: "Frontend Developer",
+      jobTitle: "Frontend Dev",
       company: "Minesoft",
       description: "Developed user-friendly web applications using React.",
       date: "Mar 2021 - Aug 2021",
     },
     {
-      jobTitle: "Fullstack Developer",
-      company: "Tata Consultancy Services",
+      jobTitle: "Fullstack Dev",
+      company: "TCS",
       description: "Worked on RESTful APIs and integrated databases.",
       date: "Sep 2021 - Present",
     },
